72403d3cf5f2fca209800c69baaeaa320518eeab
wolf4ood <enrico.risa@gmail.com>
2019-04-03 23:16:16 +0200

    Updated edge example with usage of Edge APIs

--- gremlin-client/examples/edge.rs
+++ gremlin-client/examples/edge.rs
@@ -3,13 +3,24 @@ use gremlin_client::{Edge, GremlinClient};
 fn main() -> Result<(), Box<std::error::Error>> {
     let client = GremlinClient::connect("localhost")?;
 
+    // Find outgoing edges for V[1]
     let results = client
         .execute("g.V(param).outE()", &[("param", &1)])?
         .filter_map(Result::ok)
         .map(|f| f.take::<Edge>())
         .collect::<Result<Vec<Edge>, _>>()?;
 
-    println!("{:#?}", results);
+    println!("Edges count {}", results.len());
+
+    let first = &results[0];
+
+    println!(
+        "Edge with id: [{}] and label: [{}] from: [{}] to: [{}]",
+        first.id().get::<i32>()?,
+        first.label(),
+        first.in_v().id().get::<i64>()?,
+        first.out_v().id().get::<i64>()?
+    );
 
     Ok(())
 }
