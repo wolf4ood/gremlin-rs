8c25815622c6eec973044fa6dc7de052ef9fc263
wolf4ood <enrico.risa@gmail.com>
2019-04-24 23:35:38 +0200

    started first 3 queries of chapter 3.2

--- gremlin-tutorial/src/main.rs
+++ gremlin-tutorial/src/main.rs
@@ -1,17 +1,53 @@
-use gremlin_client::{process::traversal, GremlinClient};
+use gremlin_client::{
+    process::{traversal, GraphTraversalSource},
+    GremlinClient,
+};
 
 fn main() -> Result<(), Box<std::error::Error>> {
     let client = GremlinClient::connect("localhost")?;
 
-    let result = client.execute(
-        "g.V().hasLabel('airport').
-               sample(30).
-               order().by('code').
-               local(__.values('code','city').fold()).
-               toList()",
-        &[],
-    );
     let g = traversal().with_remote(client);
+
+    chapter_32(&g)?;
+
+    Ok(())
+}
+
+fn example<T>(
+    source: &GraphTraversalSource,
+    chapter: &str,
+    description: &str,
+    fun: T,
+) -> Result<(), Box<std::error::Error>>
+where
+    T: Fn(&GraphTraversalSource) -> Result<String, Box<std::error::Error>>,
+{
+    println!("Chapter {} - {}", chapter, description);
+
+    println!("===> {}", fun(source)?);
+
+    println!("");
+
+    Ok(())
+}
+
+fn chapter_32(g: &GraphTraversalSource) -> Result<(), Box<std::error::Error>> {
+    let chapter = "3.2";
+
+    example(&g, chapter, "Find vertices that are airports", |g| {
+        let results = g.v(()).has_label("airport").to_list()?;
+        Ok(format!("Found {} airports", results.len()))
+    })?;
+
+    example(&g, chapter, "Find the DFW vertex", |g| {
+        let results = g.v(()).has("code", "DFW").to_list()?;
+        Ok(format!("Found DPW airport with id {:?}", results[0].id()))
+    })?;
+
+    example(&g, chapter, "Combining those two previous queries", |g| {
+        let results = g.v(()).has_label("airport").has("code", "DFW").to_list()?;
+        Ok(format!("Found {} airports", results.len()))
+    })?;
 
     Ok(())
 }
