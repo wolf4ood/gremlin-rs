0e1aac27e4dbc08a1814b82e41c87c182de696ab
wolf4ood <enrico.risa@gmail.com>
2019-04-28 23:09:38 +0200

    Added example of count in chapter 3.2.3

--- gremlin-tutorial/src/main.rs
+++ gremlin-tutorial/src/main.rs
@@ -14,6 +14,8 @@ fn main() -> Result<(), Box<std::error::Error>> {
 
     chapter_322(&g)?;
 
+    chapter_323(&g)?;
+
     Ok(())
 }
 
@@ -156,5 +158,56 @@ fn chapter_322(g: &GraphTraversalSource) -> Result<(), Box<std::error::Error>> {
             ))
         },
     )?;
+
+    example(
+        &g,
+        chapter,
+        "Find all the vertices that do not have a 'region' property",
+        |g| {
+            let property = "region";
+            let results = g.v(()).has_not(property).to_list()?;
+            Ok(format!(
+                "Found [{}] vertices without property {}",
+                results.len(),
+                property
+            ))
+        },
+    )?;
+    Ok(())
+}
+
+fn chapter_323(g: &GraphTraversalSource) -> Result<(), Box<std::error::Error>> {
+    let chapter = "3.2.3";
+
+    example(
+        &g,
+        chapter,
+        "How many airports are there in the graph?",
+        |g| {
+            let results = g.v(()).has_label("airport").count().to_list()?;
+            Ok(format!("Found [{}] airports", results[0]))
+        },
+    )?;
+
+    example(&g, chapter, "How many routes are there?", |g| {
+        let results = g
+            .v(())
+            .has_label("airport")
+            .out_e("route")
+            .count()
+            .to_list()?;
+        Ok(format!("Found [{}] routes", results[0]))
+    })?;
+
+    example(&g, chapter, "How many routes are there?", |g| {
+        let results = g.v(()).out_e("route").count().to_list()?;
+        Ok(format!("Found [{}] routes", results[0]))
+    })?;
+
+    example(&g, chapter, "How many routes are there?", |g| {
+        let results = g.e(()).has_label("route").count().to_list()?;
+        Ok(format!("Found [{}] routes", results[0]))
+    })?;
+
     Ok(())
 }
