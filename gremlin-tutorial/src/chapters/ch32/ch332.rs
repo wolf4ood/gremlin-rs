2f2aa835afdb33944e87c41f5f3b3678b2483e62
wolf4ood <enrico.risa@gmail.com>
2019-05-08 00:27:56 +0200

    added example with path and limit in chapter 3.3.2

--- gremlin-tutorial/src/chapters/ch32/ch332.rs
+++ gremlin-tutorial/src/chapters/ch32/ch332.rs
@@ -1,5 +1,5 @@
 use crate::chapters::example;
-use gremlin_client::process::traversal::GraphTraversalSource;
+use gremlin_client::process::traversal::{GraphTraversalSource, __};
 
 pub fn chapter_332(g: &GraphTraversalSource) -> Result<(), Box<std::error::Error>> {
     let chapter = "3.3.2";
@@ -69,6 +69,61 @@ pub fn chapter_332(g: &GraphTraversalSource) -> Result<(), Box<std::error::Error
                 .by("code")
                 .by("dist")
                 .by("city")
+                .to_list()?;
+
+            Ok(format!("Found [{:?}] path", results[0]))
+        },
+    )?;
+
+    example(
+        &g,
+        chapter,
+        "Codes for the airports we visited along with a number of runways the second airport has.",
+        |g| {
+            let results = g
+                .v(())
+                .has(("airport", "code", "LCY"))
+                .out(())
+                .limit(5)
+                .values("runways")
+                .path()
+                .by("code")
+                .by("code")
+                .by(())
+                .to_list()?;
+
+            Ok(format!("Found [{:?}] path", results[0]))
+        },
+    )?;
+
+    example(
+        &g,
+        chapter,
+        "Finds five routes that start in Austin and creates a path result containing the airport code and city name for both the source and destination airports.",
+        |g| {
+            let results = g
+                .v(3)
+                .out(())
+                .limit(5)
+                .path()
+                .by(__.values(vec!["code","city"]).fold())
+                .to_list()?;
+
+            Ok(format!("Found [{:?}] path", results[0]))
+        },
+    )?;
+
+    example(
+        &g,
+        chapter,
+        " Counts the number of outgoing routes for the source and destination airports as part of generating the path result.",
+        |g| {
+            let results = g
+                .v(3)
+                .out(())
+                .limit(5)
+                .path()
+                .by(__.out(()).count().fold())
                 .to_list()?;
 
             Ok(format!("Found [{:?}] path", results[0]))
