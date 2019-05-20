1524b8591436e2b46f14a217a27550a47fcbd111
wolf4ood <enrico.risa@gmail.com>
2019-05-20 23:17:13 +0200

    added more order example on chapter 3.12

--- gremlin-tutorial/src/chapters/ch312/mod.rs
+++ gremlin-tutorial/src/chapters/ch312/mod.rs
@@ -7,7 +7,7 @@ fn chapter_312(g: &GraphTraversalSource) -> Result<(), Box<std::error::Error>> {
     example(
         &g,
         chapter,
-        " Sort the first 20 airports returned in ascending order",
+        "Sort the first 20 airports returned in ascending order",
         |g| {
             let results = g
                 .v(())
@@ -17,6 +17,44 @@ fn chapter_312(g: &GraphTraversalSource) -> Result<(), Box<std::error::Error>> {
                 .order(())
                 .fold()
                 .to_list()?;
+            Ok(format!("Found {:?} ", results[0]))
+        },
+    )?;
+
+    example(
+        &g,
+        chapter,
+        "Sort all of the airports in the graph by their code and then return the first 20",
+        |g| {
+            let results = g
+                .v(())
+                .has_label("airport")
+                .order(())
+                .by("code")
+                .limit(20)
+                .values("code")
+                .fold()
+                .to_list()?;
+
+            Ok(format!("Found {:?} ", results[0]))
+        },
+    )?;
+
+    example(
+        &g,
+        chapter,
+        "Sort all of the places you can fly to from Austin (AUS)",
+        |g| {
+            let results = g
+                .v(())
+                .has(("code", "AUS"))
+                .out(())
+                .order(())
+                .by("code")
+                .values(vec!["code", "icao"])
+                .fold()
+                .to_list()?;
+
             Ok(format!("Found {:?} ", results[0]))
         },
     )?;
