7b11e5803a38f3aae9e2ee86341e1bca7304b307
wolf4ood <enrico.risa@gmail.com>
2019-12-18 23:20:48 +0100

    updated example in lib.rs

--- gremlin-client/src/lib.rs
+++ gremlin-client/src/lib.rs
@@ -9,7 +9,16 @@
 //! gremlin-client = "*"
 //! ```
 //!
+//! For async support via [async-std](https://async.rs/)
+//! ```toml
+//! [dependencies]
+//! gremlin-client =  { version = "*", features = ["async_std"] }
+//! ```
+//!
+//!
 //! Here it is an usage example:
+//!
+//! **Synchronous**
 //!
 //! ```rust,no_run
 //!     
@@ -32,7 +41,35 @@
 //!
 //! ```
 //!
+//! **Asynchronous**
+//!
+//! ```rust,no_run,ignore
+//!     
+//! use gremlin_client::{aio::GremlinClient, Vertex};
+//! use async_std::task;
+//! use async_std::prelude::*;
+//!
+//! fn main() -> Result<(), Box<std::error::Error>> {
+//!
+//!    task::block_on(async {
+//!     let client = GremlinClient::connect("localhost").await?;
+//!     let results = client
+//!            .execute("g.V(param)", &[("param", &1)]).await?
+//!         .filter_map(Result::ok)
+//!         .map(|f| f.take::<Vertex>())
+//!         .collect::<Result<Vec<Vertex>, _>>().await?;
+//!         println!("{:?}", results);
+//!         Ok(())
+//!    })    
+//!
+//!}
+//!
+//!
+//! ```
+//!
 //! Here it is an example with traversal:
+//!
+//! **Synchronous**
 //!
 //! ```rust,no_run
 //!     
@@ -49,8 +86,30 @@
 //!    Ok(())
 //!}
 //!
-//!
 //! ```
+//! **Aynchronous**
+//!
+//! ```rust,no_run,ignore
+//!     
+//! use gremlin_client::{aio::GremlinClient, Vertex, process::traversal::traversal};
+//! use async_std::task;
+//! use async_std::prelude::*;
+//!
+//! fn main() -> Result<(), Box<std::error::Error>> {
+//!
+//!     task::block_on(async {
+//!
+//!         let client = GremlinClient::connect("localhost").await?;
+//!
+//!         let g = traversal().with_async_remote(client);
+//!
+//!         let results = g.v(()).has_label("person").has(("name","Jon")).to_list().await?;   
+//!    
+//!         println!("{:?}", results);
+//!         Ok(())
+//!    })
+//!}
+//!
 
 mod client;
 mod connection;
