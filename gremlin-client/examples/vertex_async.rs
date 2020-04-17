8d0cc2c19890f4f55bb84928f877f35ce320e9d6
wolf4ood <maggiolo00@gmail.com>
2020-04-17 23:05:47 +0200

    Fixed fmt

--- gremlin-client/examples/vertex_async.rs
+++ gremlin-client/examples/vertex_async.rs
@@ -6,17 +6,18 @@ use async_std::prelude::*;
 #[cfg(feature = "tokio-runtime")]
 use tokio::stream::StreamExt;
 
-
 #[cfg_attr(feature = "async-std-runtime", async_std::main)]
 #[cfg_attr(feature = "tokio-runtime", tokio::main)]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let client = GremlinClient::connect("localhost").await?;
 
     let results = client
-        .execute("g.V(param)", &[("param", &1)]).await?
+        .execute("g.V(param)", &[("param", &1)])
+        .await?
         .filter_map(Result::ok)
         .map(|f| f.take::<Vertex>())
-        .collect::<Result<Vec<Vertex>, _>>().await?;
+        .collect::<Result<Vec<Vertex>, _>>()
+        .await?;
 
     println!("Vertex count: {}", results.len());
 
