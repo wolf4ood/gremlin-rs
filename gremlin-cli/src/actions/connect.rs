2059a3182a79e89564bde0c0083c4623c44f02b5
wolf4ood <maggiolo00@gmail.com>
2020-09-10 01:21:37 +0200

    Added structopt and first connect options [host,port]

--- gremlin-cli/src/actions/connect.rs
+++ gremlin-cli/src/actions/connect.rs
@@ -1,18 +1,25 @@
 use crate::{actions::Action, command::Command, context::GremlinContext};
-use clap::{App, AppSettings};
 use futures::FutureExt;
 use gremlin_client::aio::GremlinClient;
 
-pub struct ConnectAction(App<'static, 'static>);
+use structopt::StructOpt;
+
+use structopt::clap::AppSettings;
+
+pub struct ConnectAction;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "connect", no_version, global_settings = &[AppSettings::DisableVersion, AppSettings::NoBinaryName, AppSettings::ColoredHelp])]
+struct Connect {
+    #[structopt(short, long, default_value = "localhost")]
+    host: String,
+    #[structopt(short, long, default_value = "8182")]
+    port: u16,
+}
 
 impl ConnectAction {
     pub fn new() -> ConnectAction {
-        ConnectAction(
-            App::new("connect")
-                .setting(AppSettings::NoBinaryName)
-                .setting(AppSettings::DisableVersion)
-                .setting(AppSettings::ColoredHelp),
-        )
+        ConnectAction
     }
 }
 
@@ -27,26 +34,27 @@ impl Action for ConnectAction {
         _: String,
         args: Vec<String>,
     ) -> Vec<Command> {
-        self.0
-            .get_matches_from_safe_borrow(args[1..args.len()].to_vec())
-            .unwrap();
+        match Connect::from_iter_safe(args[0..args.len()].to_vec()) {
+            Ok(connect) => {
+                let task = |_ctx: &GremlinContext| {
+                    let future = async move {
+                        match GremlinClient::connect((connect.host.as_str(), connect.port)).await {
+                            Ok(client) => vec![
+                                Command::Update(Box::new(move |ctx| GremlinContext {
+                                    client: Some(client.clone()),
+                                    ..ctx
+                                })),
+                                Command::Print(Some("Connected!".into())),
+                            ],
+                            Err(err) => vec![Command::Print(Some(err.to_string()))],
+                        }
+                    };
+                    future.boxed()
+                };
 
-        let task = |_ctx: &GremlinContext| {
-            let future = async move {
-                match GremlinClient::connect("localhost").await {
-                    Ok(client) => vec![
-                        Command::Update(Box::new(move |ctx| GremlinContext {
-                            client: Some(client.clone()),
-                            ..ctx
-                        })),
-                        Command::Print(Some("Connected!".into())),
-                    ],
-                    Err(err) => vec![Command::Print(Some(err.to_string()))],
-                }
-            };
-            future.boxed()
-        };
-
-        vec![Command::Exec(Box::new(task))]
+                vec![Command::Exec(Box::new(task))]
+            }
+            Err(e) => vec![Command::Print(Some(format!("{}", e)))],
+        }
     }
 }
