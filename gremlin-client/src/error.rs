42ae6b73404ea29fdc6aa2b1caacb40e04c3df9d
Nathan KREMER <nathan.kremer@efrei.net>
2023-10-15 18:20:29 +0200

    fix from tungstenite error

--- gremlin-client/src/error.rs
+++ gremlin-client/src/error.rs
@@ -54,3 +54,17 @@ impl From<mobc::Error<GremlinError>> for GremlinError {
         }
     }
 }
+
+#[cfg(not(feature = "async_gremlin"))]
+impl From<tungstenite::error::Error> for GremlinError {
+    fn from(e: tungstenite::error::Error) -> GremlinError {
+        let error = match e {
+            tungstenite::error::Error::AlreadyClosed => tungstenite::error::Error::AlreadyClosed,
+            tungstenite::error::Error::ConnectionClosed => {
+                tungstenite::error::Error::ConnectionClosed
+            }
+            _ => return GremlinError::Generic(format!("Error from ws {}", e)),
+        };
+        GremlinError::WebSocket(error)
+    }
+}
