cc2728fdc6e2fedb6b59159132e6eb96fb1522f8
Allan Clements <criminosis@gmail.com>
2024-04-19 17:25:08 -0500

    Map explicitly null response status message to empty string

--- gremlin-client/src/message.rs
+++ gremlin-client/src/message.rs
@@ -1,3 +1,4 @@
+use serde::{Deserialize, Deserializer};
 use serde_derive::{Deserialize, Serialize};
 use serde_json::Value;
 use uuid::Uuid;
@@ -65,8 +66,16 @@ pub struct ResponseResult {
 pub struct ReponseStatus {
     pub code: i16,
     //Sometimes the message is omitted, default to empty string rather than panic
-    #[serde(default)]
+    #[serde(default, deserialize_with = "map_null_to_default")]
     pub message: String,
+}
+
+fn map_null_to_default<'de, D, T>(de: D) -> Result<T, D::Error>
+where
+    D: Deserializer<'de>,
+    T: Default + Deserialize<'de>,
+{
+    Option::<T>::deserialize(de).map(Option::unwrap_or_default)
 }
 
 pub fn message_with_args_v2<T>(op: String, processor: String, args: T) -> Message<T> {
@@ -105,5 +114,35 @@ pub fn message_with_args_and_uuid<T>(
         op,
         processor,
         args,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::message::ReponseStatus;
+
+    #[test]
+    fn handle_no_response_status_message() {
+        let parsed: ReponseStatus =
+            serde_json::from_str(r#"{"code": 123}"#).expect("Failed to parse test message");
+        assert_eq!(123, parsed.code);
+        assert_eq!("", parsed.message);
+    }
+
+    #[test]
+    fn handle_null_response_status_message() {
+        let parsed: ReponseStatus = serde_json::from_str(r#"{"code": 123, "message": null}"#)
+            .expect("Failed to parse test message");
+        assert_eq!(123, parsed.code);
+        assert_eq!("", parsed.message);
+    }
+
+    #[test]
+    fn handle_response_status_message() {
+        let parsed: ReponseStatus =
+            serde_json::from_str(r#"{"code": 123, "message": "Hello World"}"#)
+                .expect("Failed to parse test message");
+        assert_eq!(123, parsed.code);
+        assert_eq!("Hello World", parsed.message);
     }
 }
